0928a6bd22bd883dcde9ba4aa06596fc7d0fc3d4
mehmetkarakamis <ali.karakamis@ug.bilkent.edu.tr>
2018-12-21 14:56:17 +0300

    Update front-end

--- Project/Front-End/src/components/section.js
+++ Project/Front-End/src/components/section.js
@@ -1,91 +1,93 @@
 import React from "react";
 
-const NODE_PORT = 5000;
+const PYTHON_IP = "139.179.55.24";
+const PYTHON_PORT = 5000;
 
 class Section extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			section_header: undefined,
-			data: undefined,
-			clues: undefined
+			idle: undefined,
+			table: undefined,
+			clues: undefined,
+			solution: undefined
 		}
 	}
-	systemIDLE = () => {
-		let idle =
-		<div>
-			<div className="section_box w3-card-4 margin_right_for_box">
-				<h1>Front-End</h1>
-				<h4>Ready!</h4>
-			</div>
-			<div className="section_box w3-card-4 margin_right_for_box">
-				<h1>Back-End</h1>
-				<h4>Ready!</h4>
-			</div>
-			<div className="section_box w3-card-4 margin_right_for_box">
-				<h1>Puzzles</h1>
-				<h4>There are 5 old puzzles available!</h4>
-			</div>
-			<div className="section_box w3-card-4">
-				<h1>Team</h1>
-				<h4>We have 5 contributors!</h4>
-			</div>
-		</div>;
-		this.setState({section_header: undefined});
-		this.setState({data: idle});
-		this.setState({clues: undefined});
+	closeSolution = () => {
+		let solution =
+		<button onClick={this.showSolution} className="w3-button w3-green w3-hover-blue">
+			Show Solution
+		</button>;
+		this.setState({solution: solution});
+	}
+	showSolution = () => {
+		let solution = [];
+		solution.push(
+		<button onClick={this.closeSolution} className="w3-button w3-red w3-hover-blue">
+			Close Solution
+		</button>
+		);
+		
+		const [, day, month, year] = this.props.type;
+		let img_url = `http://${PYTHON_IP}:${PYTHON_PORT}/static/answers/${day}_${month}_${year}.jpeg`;
+		solution.push(
+			<img id="solution_img" src={img_url} alt="Solution screen shot"></img>
+		);
+		
+		this.setState({solution: solution});
 	}
 	drawTable = (response) => {
-		// Render date
-		const [, day, month, year] = this.props.type;
-		let section_header = [];
-		section_header.push(
-			<div id="main-day">
-				<strong>{response.day}</strong>
-				<span> {response.date}</span>
-				<br></br>
-				<a id="answers" href='#' onClick={()=> window.open(`http://localhost:${NODE_PORT}/answers/${day}_${month}_${year}.jpeg`, "_blank")}>
-					Show the solution
-				</a>
-			</div>
-		);
-		this.setState({section_header: section_header});
-		// Render table
+		// Clear idle
+		this.setState({idle: undefined});
+
+		// Declare date
+		const day = response.day;
+		const date = response.date;
+
+		// Render table begins!
 		let table = [];
-		let children = [];
+		let temp = []; // is a table cells
 		let cells = 0;
-		for(let k = 0; k < 5; k++) {
-			for(let i = 0; i < 5; i++) {
+
+		let i, k;
+		for(k = 0; k < 5; k++) {
+			for(i = 0; i < 5; i++) {
 				if(response.cells[cells] === "-1") {
-					children.push(
-						<td className="black">&nbsp;</td>
-					);
+					temp.push(<td className="black">&nbsp;</td>);
 				}
 				else if(response.cells[cells] === "0") {
-					children.push(
-						<td>&nbsp;</td>
-					);
+					temp.push(<td>&nbsp;</td>);
 				}
 				else {
-					children.push(
-						<td className="w3-display-container"><a className="w3-display-topleft">{response.cells[cells]}</a></td>
-					);
+					temp.push(<td className="w3-display-container">
+									<a className="w3-display-topleft">{response.cells[cells]}</a>
+								</td>);
 				}
 				cells++;
 			}
-			table.push(<tr>{children}</tr>);
-			children = [];
+			table.push(<tr>{temp}</tr>);
+			temp = [];
 		}
-		this.setState({data: <table id="main-table">{table}</table>});
-		// Render Clues
+		// add date above table
+		let table_with_date = 
+		<div>
+			<h1 id="show-date">{day}, {date}</h1>
+			<table id="main-table">{table}</table>
+		</div>;
+
+		this.setState({table: table_with_date});
+
+		// Render clues begins!
 		let clues = [];
 		let across = [];
 		let down = [];
-		let i = 0;
+		// across
+		i = 0;
 		while(i < response.across.length) {
 			across.push(<p><strong>{response.across[i].no}.</strong> {response.across[i].text}</p>);
 			i++;
 		}
+		// down
 		i = 0;
 		while(i < response.down.length) {
 			down.push(<p><strong>{response.down[i].no}.</strong> {response.down[i].text}</p>);
@@ -94,10 +96,22 @@ class Section extends React.Component {
 		clues.push(<div id="across"><h1>ACROSS</h1>{across}</div>);
 		clues.push(<div id="down"><h1>DOWN</h1>{down}</div>);
 		this.setState({clues: clues});
-	}
 
-	fetchData = (day,month,year) => {
-		const url = `http://localhost:${NODE_PORT}/old/${day}/${month}/${year}`;
+		// Render solution begins!
+		let solution =
+		<button onClick={this.showSolution} id="showSol" className="w3-button w3-green w3-hover-blue">
+			Show Solution
+		</button>;
+		this.setState({solution: solution});
+	}
+	fetchData = (day, month, year) => {
+		let url = null;
+		if(this.props.type === "today") {
+			url = `http://${PYTHON_IP}:${PYTHON_PORT}/static/data/today.json`;
+		}
+		else {
+			url = `http://${PYTHON_IP}:${PYTHON_PORT}/static/data/${day}_${month}_${year}.json`;
+		}
 		fetch(url)
 			.then(response => response.json())
 			.then(responseJSON => {
@@ -109,11 +123,11 @@ class Section extends React.Component {
 				console.log("$ print-old");
 			});
 	}
-
+	// If we have console command
 	componentDidUpdate = (prevProps) => {
 		// Get today's data
 		if(this.props.type === "today" && prevProps.type !== "today") {
-			this.fetchData("05","11","2018");
+			this.fetchData("17", "12", "2018");
 		}
 		// Get old data
 		else {
@@ -125,21 +139,55 @@ class Section extends React.Component {
 				console.log("Example:\nget-old 28 10 2018");
 			}
 			else if (prevDay !== day || prevMonth !== month || prevYear !== year) {
-				this.fetchData(day,month,year);
+				this.fetchData(day, month, year);
 			}
 		}
 	}
-
+	// Show boxes if idle
+	systemIDLE = () => {
+		let idle =
+		<div id="display-idle">
+			<div className="section-box w3-card-4 margin-right-for-box">
+				<h1>Front-End</h1>
+				<h4>Ready!</h4>
+			</div>
+			<div className="section-box w3-card-4 margin-right-for-box">
+				<h1>Back-End</h1>
+				<h4>Ready!</h4>
+			</div>
+			<div className="section-box w3-card-4 margin-right-for-box">
+				<h1>Puzzles</h1>
+				<h4>There are 10 old puzzles available!</h4>
+			</div>
+			<div className="section-box w3-card-4">
+				<h1>Team</h1>
+				<h4>We have 5 contributors!</h4>
+			</div>
+		</div>;
+		this.setState({idle: idle});
+	}
+	// First function to be executed
 	componentDidMount = () => {
 		this.systemIDLE();
 	}
-
 	render() {
 		return(
 			<section id="main-section">
-				{this.state.section_header}
-				{this.state.data}
-				{this.state.clues}
+				{this.state.idle}
+				<div className="clear"></div>
+
+				<div id="display-table">
+					{this.state.table}
+				</div>
+
+				<div id="display-clues">
+					{this.state.clues}
+				</div>
+
+				<div id="display-solution">
+					{this.state.solution}
+				</div>
+
 				<div className="clear"></div>
 			</section>
 		);
--- Project/Front-End/src/index.js
+++ Project/Front-End/src/index.js
@@ -8,17 +8,72 @@ import Header from "./components/header.js";
 import Section from "./components/section.js";
 
 // CSS
-import "./public/css/w3.css";
 import "./public/css/custom.css";
 
 // Globals
+const PYTHON_IP = "139.179.55.24";
+const PYTHON_PORT = 5000;
 const old_puzzles = [
-	"28 10 2018\n",
-	"29 10 2018\n",
-	"30 10 2018\n",
+	"08 11 2018\n",
+	"05 11 2018\n",
+	"01 11 2018\n",
 	"31 10 2018\n",
-	"01 11 2018\n"
+	"30 10 2018\n",
+	"29 10 2018\n",
+	"28 10 2018\n",
+	"01 06 2016\n",
+	"02 10 2015\n",
+	"23 10 2014\n"
 ];
+function render_answers(args) {
+	const [, day, month, year] = args;
+
+	let url_cells = `http://${PYTHON_IP}:${PYTHON_PORT}/static/data/${day}_${month}_${year}.json`;
+	let url_answers = `http://${PYTHON_IP}:${PYTHON_PORT}/static/${day}_${month}_${year}answer.json`;
+
+	fetch(url_cells)
+	.then(response => response.json())
+	.then(response_cells => {
+		fetch(url_answers)
+		.then(response => response.json())
+		.then(response_answers => {
+			for(let i = 0; i < 25; i += 5) {
+				let cell = i/5;
+				cell = "A_"+String(cell);
+				let single_answer = response_answers[cell];
+	
+				console.log(single_answer);
+				for(let j = 0; j < 5; j++) {
+					if(single_answer.length !== 0) {
+						if(response_cells.cells[i+j] !== "-1") {
+							const table = document.getElementById("main-table").getElementsByTagName("td")[i+j];
+							table.innerHTML = "<span>"+single_answer[j]+"</span>";
+						}
+					}
+				}
+			}
+		})
+	})
+}
+function connect_server(args) {
+	let connect_back_end = `http://${PYTHON_IP}:${PYTHON_PORT}/static/state.json`
+	fetch(connect_back_end)
+	.then(response => response.json())
+	.then(responseJSON => {
+		if(responseJSON.state[0] === "DONE!") {
+			console.log("Puzzle solved! Trying to render answers...");
+			clearInterval(window.timer);
+			//let answers = get_answers(args);
+			render_answers(args);
+		}
+		else {
+			console.log(responseJSON.state[0]);
+		}
+	})
+	.catch(error => {
+		console.log("Trying to connect back-end server....");
+	});
+}
 // Render
 const react_root = document.getElementById("root");
 function renderReactDOM(section_state) {
@@ -30,24 +85,37 @@ function renderReactDOM(section_state) {
 			hideTopBar = {true}
 			allowTabs = {true}
 			startState = {'maximised'}
-			style={{ fontWeight: "bold", fontSize: "1em", height: "350px"}}
+			style={{ fontWeight: "bold", fontSize: "1em", height: "375px"}}
 			commands={{
-				"get-today": (args, print, runCommand) => {
-					console.log("Getting today's puzzle!");
-					console.log("Trying to connect NYTIMES...");
-					setTimeout(function(){
-						renderReactDOM("today");
-						console.log("Today's puzzle fetched successfully!");
-				  	}, 3000);
-				},
 				"print-old": (args, print, runCommand) => {
 					print("Old puzzle list:");
 					print(old_puzzles);
-					print("You can run an old puzzle by typing:");
-					print("get-old 28 10 2018");
+				},
+				"get-today": (args, print, runCommand) => {
+					print("Trying to connect NYTimes...");
+					print("Getting clues...");
+					setTimeout(function(){ renderReactDOM("today");; }, 6000);
 				},
 				"get-old": (args, print, runCommand) => {
+					print("Getting old puzzle....");
+					print("Loading....");
 					renderReactDOM(args);
+				},
+				"solve": (args, print, runCommand) => {
+					const [, day, month, year] = args;
+					// Send request to solve
+					var http = new XMLHttpRequest();
+					var url = `http://${PYTHON_IP}:${PYTHON_PORT}/solve/${day}/${month}/${year}`;
+					http.open("GET", url, true);
+					http.setRequestHeader("Content-Type", "application/json");
+					http.send();
+
+					window.timer = setInterval(function(){
+						connect_server(args);
+					}, 195);
+				},
+				"clear": (args, print, runCommand) => {
+					window.location.reload(true);
 				}
 			}}
 			descriptions={{
@@ -56,7 +124,8 @@ function renderReactDOM(section_state) {
 				help: "Lists all the commands and shows their explanations",
 				"get-today": "This command gets the today's puzzle",
 				"print-old": "This command gets the list of old puzzles",
-				"get-old": "This command gets a specific old puzzle"
+				"get-old": "This command gets a specific old puzzle",
+				"solve-it": "Solves the displayed puzzle"
 			}}
 			msg='Welcome to Project Dolphin. To get help type: help'
 		/>
